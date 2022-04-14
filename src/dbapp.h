5ec897a8f33d651dbf38f73029545c0df3135552
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-04-14 16:00:40 +0200

    Define class Student Skeleton

--- src/dbapp.h
+++ src/dbapp.h
@@ -13,13 +13,13 @@ protected:
     std::string name;
     std::string city;
 
-    std::ostream& write_legibly(std::ostream& strm) const;
+    virtual std::ostream& write_legibly(std::ostream& strm) const;
 
     friend std::ostream& operator<<(std::ostream& strm, const Personal& perso){
         return perso.write_legibly(strm);
     }
 
-    std::istream& readfrom_console(std::istream& strm);
+    virtual std::istream& read_from_console(std::istream& strm);
 
     friend std::istream& operator>>(std::istream& strm, Personal& perso){
         return perso.readfrom_console(strm);
@@ -27,17 +27,45 @@ protected:
 public:
     Personal();
     Personal(std::string, std::string, std::string, int, long);
-    void write_to_file(std::fstream& stream) const;
-    void read_from_file(std::fstream& stream);
+    virtual void write_to_file(std::fstream& stream) const;
+    virtual void read_from_file(std::fstream& stream);
     void read_key();
 
     // ---
-    int size() const {
+    virtual int size() const {
         return 9 + name.size() + city.size() + sizeof(year) + sizeof(salary);
     }
 
     friend bool operator==(const Personal& lhs, const Personal& rhs){
         return lhs.ssn == rhs.ssn;
+    }
+};
+
+// -----
+class Student: public Personal {
+protected:
+    std::string major;
+
+    virtual std::ostream& write_legibly(std::ostream& stream) const;
+
+    friend std::ostream& operator<<(std::ostream& strm, Student& student){
+        return student.write_legibly(strm);
+    }
+
+    virtual std::istream& read_from_console(std::istream& strm);
+
+    friend std::istream& operator>>(std::istream& strm, Student& student){
+        return student.read_from_console(strm);
+    }
+
+public:
+    Student();
+    Student(std::string, std::string, int, long, std::string);
+
+    void write_to_file(std::fstream& strm) const override;
+    void read_from_file(std::fstream& strm) override;
+    int size() const override{
+        return Personal::size() + major.size();
     }
 };
 
