60069c23eb58419c380fbadc43dc68f399ab47d9
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-04-16 21:37:28 +0200

    Implement Network::read_committees(std::string)

--- src/distinctRepr.cpp
+++ src/distinctRepr.cpp
@@ -115,6 +115,13 @@ class LocalTree: public BST<NetTreeNode>{
         (*(node->item.verticesPtr))[node->item.idnum].adjacent =
             node->item.adjacent;
     }
+
+    void print_tree(){
+        if(root == nullptr){ return;}
+        print_tree(root->left);
+        std::cout << root;
+        print_tree(root->right);
+    }
 }
 
 // ----------------------------------------------------------------------
@@ -126,7 +133,7 @@ public:
         verticePtr = new VertexArrayRecord*;
     }
 
-    void read_committees(std::string& committees);
+    void read_committees(std::string committees);
     void ford_fulkerson_max_flow();
 
 
@@ -200,4 +207,116 @@ std::ostream& operator<<(std::ostream& out, const Network& net){
     }
 
     return out;
+}
+
+// ----------------------------------------------------------------------
+// ----- Network::read_committees(std::string)                      -----
+// ----------------------------------------------------------------------
+void Network::read_committees(std::string filename){
+    char i, name[80], *s;
+    LocalTree commTree;
+    LocalTree membTree;
+    Vertex membVer(0, 1, 0, false);
+    Vertex commVer(0, 1, 0, true);
+    Vertex *commVerAddr;
+    Vertex *membVerAddr;
+    NetTreeNode commTreeNode(verticePtr);
+    NetTreeNode membTreeNode(verticePtr);
+    NetTreeNode *member;
+    std::list<Vertex> *srcList = new std::list<Vertex>;
+    std::ifstream fin(filename);
+    if(fin.fail()){
+        std::cerr << "Cannot open " << filename << std::endl;
+        exit(-1);
+    }
+
+    while(!fin.eof()){
+        fin >> name[0];     // skip leading spaces
+        if(fin.eof()){ break;}
+        for(i=0; name[i] != ':'){
+            name[++i] = fin.get();
+        }
+        for(i--; isspace(name[i]); i--){ /* discard trailing spaces */ }
+        name[i+1] = '\0';
+        s = strdup(name);
+        //!@todo maybe node need for s as char[] but as std::string
+        commTreeNode.idnum = commVer.idnum = nvertices++;
+        commTreeNode.idname = s;    /** @todo string(name) ??? */
+        for(bool lastMember = false; lastMember=false; ){
+            fin >> name[0]; // skip leading spaces
+            for(i=0; name[i] != ',' && name[i] != ';'; ){
+                name[++i] = fin.get();
+            }
+            if(name[i] == ';'){ lastMember = true; }
+            for(i--; isspace(name[i]); i--){ /* discard trailing spaces */ }
+            name[i+1] = '\0';
+            s = strdup(name);
+            membTreeNode.idname = s;
+            commVer.forward = false;
+            if((member = membTree.search(membTreeNode)) == nullptr){
+                membVer.idnum = membTreeNode.idnum == nvertices++;
+                membTreeNode.adjacent->push_front(Vertex(sink, 1, 0, true));
+                membTreeNode.adjacent->push_front(commVer);
+                membTree.insert(membTreeNode);
+                membTreeNode.adjacent = new std::list<Vertex>;
+            }else{
+                membVer.idnum = member->idnum;
+                member->adjacent->push_front(commVer);
+                commVerAddr = &*member->adjacent->begin();
+            }
+            membVer.forward = true;
+            commTreeNode.adjacent->push_front(membVer);
+            membVerAddr = &*commTreeNode.adjacent->begin();
+            membVerAddr->twin = commVerAddr;
+            commVerAddr->twin = membVerAddr;
+        }
+        commVer.forward = true;
+        srcList->push_front(commVer);
+        commTree.insert(commTreeNode);
+        commTreeNode.adjacent = new std::list<Vertex>;
+    }
+    fin.close();
+    std::cout << std::endl << "Committee tree:\n";
+    commTree.print_tree();
+    std::cout << std::endl << "Member tree\n";
+    membTree.print_tree();
+    vertices = *verticePtr = new VertexArrayRecord[nvertices];
+    if(vertices == nullptr){
+        std::cerr << "Not enough memory" << std::endl;
+        exit(-1);
+    }
+    vertices[source].idname = "source";
+    vertices[sink].idname = "sink";
+    vertices[source].adjacent = srcList;
+    vertices[source].parent = none;
+    commTree.inorder();
+    membTree.inorder();
+}
+
+// ----------------------------------------------------------------------
+// ----- Network::label(Vertex*, int)                               -----
+// ----------------------------------------------------------------------
+void Network::label(Vertex *vertex, int val){
+    /** @todo */
+}
+
+// ----------------------------------------------------------------------
+// ----- Network::augment_path()                                    -----
+// ----------------------------------------------------------------------
+void Network::augment_path(){
+    /** @todo */
+}
+
+// ----------------------------------------------------------------------
+// ----- Network::ford_fulkerson_max_flow()                         -----
+// ----------------------------------------------------------------------
+void Network::ford_fulkerson_max_flow(){
+    /** @todo */
+}
+
+// ------------------------
+// ----- MAIN  DRIVER -----
+// ------------------------
+int main(int argc, char **argv){
+    /** @todo */
 }
