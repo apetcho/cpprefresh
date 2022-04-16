9eeb7c8c75002ff38ab80b1580452faff2dbc4c5
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-04-17 01:54:41 +0200

    Implement AccountFile::append() method

--- src/account.cpp
+++ src/account.cpp
@@ -21,3 +21,22 @@ std::istream& DepositAccount::read(std::istream& strm){
     strm.read((char*)&deb, sizeof(deb));
     return strm;
 }
+
+// --------------------------------------------------------------------
+// ----- AccountFile::append()                                    -----
+// --------------------------------------------------------------------
+long AccountFile::append(Account& account) throw(WriteError){
+    stream.seekp(0L, std::ios::end);    // Seek to end
+    long pos = stream.tellg();          // Save the position
+
+    if(!stream){ throw WriteError(name); }
+
+    AccountType accId = account.get_account_type();
+    int id = (int)accId;
+    stream.write((char*)&id, sizeof(id));
+    if(!stream){ throw WriteError(name); }
+    else{ account.write(stream); }
+
+    if(!stream){ throw WriteError(name); }
+    else{ return pos; }
+}
