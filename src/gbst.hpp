8acf27eac88751f2d19750ecf6b461be6fa4e370
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-04-16 19:46:19 +0200

    Implement BST<T>::balance() method

--- src/gbst.hpp
+++ src/gbst.hpp
@@ -523,3 +523,16 @@ void BST<T>::morris_postorder(){
         }
     }//
 }
+
+// ----------------------------------------------------------------------
+// ----- BST<T>::balance(T*, int, int)                             -----
+// ----------------------------------------------------------------------
+template<typename T>
+void BST<T>::balance(T data[], int first, int last){
+    if(first <= last){
+        int middle = (first + last)/2;
+        insert(data[middle]);
+        balance(data, first, middle-1);
+        balance(data, middle+1, last);
+    }
+}
