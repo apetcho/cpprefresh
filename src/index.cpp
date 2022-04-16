a2f30dc7ddd22e07c43408b5e13b35d8f1ad5524
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-04-17 00:25:58 +0200

    Implement IndexEntry::read_at()

--- src/index.cpp
+++ src/index.cpp
@@ -2,10 +2,18 @@
 
 /** @todo ... */
 
-/** */
+/** @todo add documentation */
 std::fstream& IndexEntry::write_at(std::fstream& strm, long pos) const{
     strm.seekg(pos);
     strm.write((char*)&key, sizeof(key));
     strm.write((char*)&recNr, sizeof(recNr));
+    return strm;
+}
+
+/** @todo add documentation */
+std::fstream& IndexEntry::read_at(std::fstream& strm, long pos){
+    strm.seekg(pos);
+    strm.read((char*)&key, sizeof(key));
+    strm.read((char*)&recNr, sizeof(recNr));
     return strm;
 }
