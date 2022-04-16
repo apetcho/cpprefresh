73b179b19e1518a6f13da3970302b184aff9f61d
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-04-16 17:44:12 +0200

    Adapt to std::stack to a customized generic Stack

--- src/gbst.hpp
+++ src/gbst.hpp
@@ -1,4 +1,17 @@
 #ifndef __MY_GENERIC_BST_H_
 #define __MY_GENERIC_BST_H_
 
+#include<queue>
+#include<stack>
+
+template<typename T>
+class Stack : std::stack<T>{
+public:
+    T pop(){
+        T = tmp = top();
+        std::stack<T>::pop();
+        return tmp;
+    }
+};
+
 #endif
